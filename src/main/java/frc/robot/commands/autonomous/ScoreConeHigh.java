784a095428905d418393136dba89c32344312327
UserC2 <aj01cars@outlook.com>
2023-03-22 19:35:58 -0400

    Disable vision for autonomous mode

--- src/main/java/frc/robot/commands/autonomous/ScoreConeHigh.java
+++ src/main/java/frc/robot/commands/autonomous/ScoreConeHigh.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.MoveArmToHigh;
-import frc.robot.commands.VisionAlign;
+// import frc.robot.commands.VisionAlign;
 import frc.robot.subsystems.ArmGripper;
 import frc.robot.subsystems.SwerveDrive;
 
@@ -18,7 +18,7 @@ public class ScoreConeHigh extends SequentialCommandGroup {
   public ScoreConeHigh(SwerveDrive drive, ArmGripper arm) {
     addCommands(
         new ParallelCommandGroup(
-            new VisionAlign(drive, null),
+            // new VisionAlign(drive, null),
             new MoveArmToHigh(arm)
         ),
         Commands.waitSeconds(0.5),
