f3f59ae69bc9047073c1cdeeaba9f49c64585ba2
UserC2 <aj01cars@outlook.com>
2023-03-27 17:16:34 -0400

    Update arm gear ratio

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,8 +74,8 @@ public final class Constants {
             public static final double EXTENSION_MOTOR = UltraPlanetaryRatios.FIVE_TO_ONE * UltraPlanetaryRatios.FOUR_TO_ONE;
             /** Ratio between shaft pulley (18t) and arm pulley (48t). */
             public static final double LOWER_ARM_CHAIN = 18.0 / 48.0;
-            /** Arm shaft rotations per motor rotation. (2x 5:1 + 1x 3:1) */
-            public static final double LOWER_ARM_MOTOR = (1.0 / 5.0) * (1.0 / 5.0) * (1.0 / 3.0);
+            /** Arm shaft rotations per motor rotation. (45:1) */
+            public static final double LOWER_ARM_MOTOR = 1.0 / 45.0;
             /** Total ratio between lower arm motor and lower arm rotation. */
             public static final double LOWER_ARM = LOWER_ARM_MOTOR * LOWER_ARM_CHAIN;
             /** Ratio between shaft pulley (18t) and arm pulley (48t). */
