add82cd4e8c089dbbd9143b88920cd6bf078927b
UserC2 <aj01cars@outlook.com>
2023-04-01 16:26:13 -0400

    Formatting

--- src/main/java/frc/robot/commands/StowMidToHigh.java
+++ src/main/java/frc/robot/commands/StowMidToHigh.java
@@ -7,14 +7,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ArmGripper;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class StowMidToHigh extends SequentialCommandGroup {
   /** Creates a new StowMidToHigh. */
   public StowMidToHigh(ArmGripper gripper) {
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new MoveArmProfiled(gripper, "LongThrowPickup",false),new MoveArmProfiled(gripper, "PickupToHigh",false));
+    addCommands(
+      new MoveArmProfiled(gripper, "LongThrowPickup", false),
+      new MoveArmProfiled(gripper, "PickupToHigh", false)
+    );
   }
 }
