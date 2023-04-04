dfed38b8c21e066ab86ca286e639f9c5b92172a8
UserC2 <aj01cars@outlook.com>
2023-04-04 16:35:23 -0400

    Remove completed TODO comment

--- src/main/java/frc/robot/subsystems/ArmGripper.java
+++ src/main/java/frc/robot/subsystems/ArmGripper.java
@@ -2,8 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-     // TODO: clean up encoder calculations and encoder calculation functions
-
 package frc.robot.subsystems;
 
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
