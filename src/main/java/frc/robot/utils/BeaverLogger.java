bb07451862f1cfb06143d0368a393594da7db111
UserC2 <85082371+UserC2@users.noreply.github.com>
2023-11-08 14:51:24 -0500

    Change formatting to be more consistent

--- src/main/java/frc/robot/utils/BeaverLogger.java
+++ src/main/java/frc/robot/utils/BeaverLogger.java
@@ -14,22 +14,22 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /** Add your docs here. */
 public class BeaverLogger {
   HashMap<String, Supplier<Double>> loggingMap = new HashMap<String, Supplier<Double>>();
-	HashMap<String, Supplier<Double>> smartDashboardMap = new HashMap<String, Supplier<Double>>();
-	HashMap<String, DoubleLogEntry> logEntryMap = new HashMap<String, DoubleLogEntry>();
-	final DataLog log = DataLogManager.getLog();
-	
-  public BeaverLogger() {}
-	
-  public void addLoggable(String name, Supplier<Double> supplier, boolean toSmartDash) {
-		if (toSmartDash) {
-			smartDashboardMap.put(name, supplier);
-		} else {
-			loggingMap.put(name, supplier);
-			logEntryMap.put(name, new DoubleLogEntry(log, name));
-		}
-	}
+  HashMap<String, Supplier<Double>> smartDashboardMap = new HashMap<String, Supplier<Double>>();
+  HashMap<String, DoubleLogEntry> logEntryMap = new HashMap<String, DoubleLogEntry>();
+  final DataLog log = DataLogManager.getLog();
 
-	public void logAll() {
+  public BeaverLogger() {}
+
+  public void addLoggable(String name, Supplier<Double> supplier, boolean toSmartDash) {
+    if (toSmartDash) {
+      smartDashboardMap.put(name, supplier);
+    } else {
+      loggingMap.put(name, supplier);
+      logEntryMap.put(name, new DoubleLogEntry(log, name));
+    }
+  }
+
+  public void logAll() {
     // Logging values in smartDashboardMap
     for (String key : smartDashboardMap.keySet()) {
         Double value = smartDashboardMap.get(key).get();
@@ -46,54 +46,5 @@ public class BeaverLogger {
         entry.append(value);
       }
     }
-	}
-}
-
-/*
- * 
-  private class Log {
-    private DoubleLogEntry lowerAngle, upperAngle, extensionDistance,
-                           lowerBackupAngle, upperBackupAngle,
-                           lowerSetpoint, upperSetpoint, extensionSetpoint,
-                           lowerOutput, upperOutput, extensionOutput,
-                           lowerCurrent, upperCurrent, extensionCurrent;
-    
-    private Arm arm;
-
-    public Log(Arm arm) {
-      final DataLog log = DataLogManager.getLog();
-      lowerSetpoint = new DoubleLogEntry(log, "/arm/setpoints/lower");
-      upperSetpoint = new DoubleLogEntry(log, "/arm/setpoints/upper");
-      extensionSetpoint = new DoubleLogEntry(log, "/arm/setpoints/extension");
-  
-      lowerAngle = new DoubleLogEntry(log, "/arm/angles/lower");
-      upperAngle = new DoubleLogEntry(log, "/arm/angles/upper");
-      extensionDistance = new DoubleLogEntry(log, "/arm/angles/extension");
-  
-      lowerBackupAngle = new DoubleLogEntry(log, "arm/angles/lower_backup");
-      upperBackupAngle = new DoubleLogEntry(log, "arm/angles/upper_backup");
-  
-      lowerOutput = new DoubleLogEntry(log, "/arm/output/lower");
-      upperOutput = new DoubleLogEntry(log, "/arm/output/upper");
-      extensionOutput = new DoubleLogEntry(log, "/arm/output/extension");
-  
-      lowerCurrent = new DoubleLogEntry(log, "/arm/current/lower");
-      upperCurrent = new DoubleLogEntry(log, "/arm/current/upper");
-      extensionCurrent = new DoubleLogEntry(log, "/arm/current/extension");
-      
-      this.arm = arm;
-    }
-    
-    public void logAll(){
-      lowerAngle.append(arm.getLowerAngle().getDegrees());
-      upperAngle.append(arm.getUpperAngle().getDegrees());
-      extensionDistance.append(arm.getExtensionDistance());
-      lowerBackupAngle.append(arm.getLowerBackupAngle().getDegrees());
-      upperBackupAngle.append(arm.getUpperBackupAngle().getDegrees());
-      lowerOutput.append(1.0); // current PID output
-      upperOutput.append(1.0); // current PID output
-      extensionOutput.append(1.0); // current PID output
-
-    }
   }
- */
+}
