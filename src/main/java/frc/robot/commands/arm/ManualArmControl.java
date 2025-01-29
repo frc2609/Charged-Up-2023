c828af8b52e0b99f55057e2a49230e9465003738
RIVERsWITHERS <riversarebrurning@gmail.com>
2025-01-28 20:22:25 -0500

    Road runner code but imported to 2025

--- src/main/java/frc/robot/commands/arm/ManualArmControl.java
+++ src/main/java/frc/robot/commands/arm/ManualArmControl.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.Xbox;
-import frc.robot.subsystems.Arm;
+//import frc.robot.subsystems.Arm;
 
 /**
  * Adjust the arm setpoints proportional to the provided inputs.
